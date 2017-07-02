61365e5bc132099e40633c0853bc1246c1e29283
fsemprini <federico.semprini@iit.it>
2017-07-02 16:51:28 +0200

    Added data selection on app2

--- project/app2.js
+++ project/app2.js
@@ -4,6 +4,15 @@
     let data = {};
     let regions = [];
     let italyMap = {};
+    let years = [];
+    let selections = [
+        'starts',
+        'voucher'
+    ];
+    let currentDimensions = {
+        year: 2013,
+        selection: 'starts'
+    };
     let mapSizes = {
         width: 500,
         height: 550
@@ -12,7 +21,8 @@
     d3.json('data/app2.json', (error, jsonData) => {
         data.starts = jsonData.starts;
         data.voucher = jsonData.voucher;
-        regions = data.regions;
+        regions = jsonData.regions;
+        years = data.voucher.map(s => s.year);
 
         d3.json('data/italy.json', (error, jsonItaly) => {
             italyMap = jsonItaly;
@@ -22,6 +32,7 @@
 
 
     function init() {
+        initCmds();
         createMap();
 
     }
@@ -35,14 +46,14 @@
         updateMap('starts', 2013);
     }
 
-    function updateMap(dimension, year) {
-        const currentData = data[dimension].find(d => d.year === year).data;
+    function updateMap(selection, year) {
+        const currentData = data[selection].find(d => d.year === getNum(year)).data;
         const topoMap = topojson.feature(italyMap, italyMap.objects.reg2011).features;
 
         const map = d3.select('#app2-map').select('svg');
 
         const color = d3.scaleQuantize()
-            .domain([0, removeTotal(currentData)])
+            .domain([d3.min(removeTotals(currentData)), d3.max(removeTotals(currentData))])
             .range(["#ffffbf", "#d9ef8b", "#91cf60", "#1a9850"]);
 
         const projection = d3.geoMercator()
@@ -51,18 +62,86 @@
         const path = d3.geoPath()
             .projection(projection);
 
-        const regions = map.selectAll(".region")
+        const reg = map.selectAll(".region")
             .data(topoMap);
 
-        regions.enter()
-            .insert("path")
+        var tip = d3.tip()
+            .attr('class', 'tip')
+            .offset([-8, 0])
+            .html(d => getRegion(d.properties.COD_REG).name + '<br>' + getData(d));
+        map.call(tip);
+
+        reg.enter()
+            .append("path")
             .attr("class", "region")
             .attr("d", path)
-            .style("fill", d => color(currentData[d.properties.COD_REG - 1]));
+            .merge(reg)
+            .style("fill", d => color(getData(d)))
+            .on('mouseover', tip.show)
+            .on('mouseout', tip.hide);
+
+        function getData(d) {
+            return currentData[d.properties.COD_REG - 1];
+        }
     }
 
-    function removeTotal(d) {
-        return d3.max(d.slice(0, d.length - 2));
+    function initCmds() {
+        const cmds = d3.select('#app2-cmds');
+
+        const yearSelect = cmds.append('select')
+            .attr('id', 'year-select');
+
+        yearSelect.on('change', () => {
+            update({ year: d3.select('#year-select').node().value })
+        })
+
+        yearSelect.selectAll('option')
+            .data(years)
+            .enter()
+            .append('option')
+            .attr('value', y => y)
+            .text(y => y);
+
+        const selectionSelect = cmds.append('select')
+            .attr('id', 'selection-select');
+
+        selectionSelect.on('change', () => {
+            update({ selection: d3.select('#selection-select').node().value })
+        })
+
+        selectionSelect.selectAll('option')
+            .data(selections)
+            .enter()
+            .append('option')
+            .attr('value', y => y)
+            .text(y => y);
+    }
+
+    function update(params) {
+        if (params.selection)
+            currentDimensions.selection = params.selection;
+        if (params.year)
+            currentDimensions.year = params.year;
+
+        updateMap(currentDimensions.selection, currentDimensions.year);
+    }
+
+    function getRegion(cod) {
+        return regions.find(r => r.cod === cod);
+    }
+
+    function removeTotals(d) {
+        return d.slice(0, d.length - 2);
+    }
+
+    function getNum(str) {
+        if (typeof str !== 'string') {
+            if (typeof str === 'number')
+                return str;
+            return 0;
+        }
+
+        return +str.replace(/\D+/g, '');
     }
 
 })();
